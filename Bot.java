e1fe396ada7cae650312a4273d29387a04246765
23MinL <posing1606@gmail.com>
2025-02-20 17:28:08 +0900

    ver2

--- Bot.java
+++ Bot.java
@@ -3,21 +3,24 @@ import java.net.http.*;
 import java.time.*;
 import java.util.*;
 
-public class Bot{
-    public static void main(String[] args){ //진입부분
-        //이게 있어야 이 클래스를 실행했을 때 작동함
-        //웹훅 만들것
-        //환경변수로 받아 -> yml 파일에 전달
+public class Bot {
+    public static void main(String[] args) { // 진입부분
+        // 이게 있어야 이 클래스를 실행했을 때 작동을 함
+        // 웹훅을 만들 거임 -> URL 필요함
+        // 환경변수로 받아올 것임 -> yml 파일에서 전달하게
         String webhookUrl = System.getenv("SLACK_WEBHOOK_URL");
-    
-        //
+        String message = System.getenv("SLACK_WEBHOOK_MSG");
+
+        // Java 11 -> fetch
         HttpClient client = HttpClient.newHttpClient();
-        //요청을 얹힐 것
+        // 요청을 얹힐 거다
         HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create(webhookUrl))
             .header("Content-Type", "application/json")
-            .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\"테스트 메시지\"}")) 
-            .build();  
+            // .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\"테스트 메시지\"}")) 
+            .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\" + " + message + "\"}")) 
+            .build();
+        
         try {
             HttpResponse<String> response = client.send(
                 request, HttpResponse.BodyHandlers.ofString()
